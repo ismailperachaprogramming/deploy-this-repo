aa1b2f467f156da7ffea981cc33273d18e0ffcb5
Ismail Peracha <91555891+ismailperachaprogramming@users.noreply.github.com>
2024-10-20 20:00:00 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 
-export default defineConfig({
-	plugins: [sveltekit(),
-		mkcert()
-	],
-	server: {
-		https: true,
-		proxy: {}, // essential to avoid "can't use Symbol where you need a string" error
-	},
-})
+// vite.config.js
+export default {
+  plugins: [],
+  server: {
+    port: 3000,
+    strictPort: true,
+  },
+};
